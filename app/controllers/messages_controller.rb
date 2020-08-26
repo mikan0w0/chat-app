f9011ed0de7924a45d7fa9796f814fff17d6003e
karasuyamamika <j.girl.mi@gmail.com>
2020-08-26 18:17:31 +0900

    ActiveStorageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,6 +18,9 @@ class MessagesController < ApplicationController
   private
 
   def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+  def message_params
     params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
